90c156021db7901cea40913407462a863c7fc6ed
Pâmela Ruchel Hartwig <45543608+pamelaruchel@users.noreply.github.com>
2025-10-18 16:08:30 -0300

    Update spotify.js

--- api/spotify.js
+++ api/spotify.js
@@ -1,10 +1,16 @@
 export default async function handler(req, res) {
+  // CORS
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") return res.status(200).end();
+
   const refresh_token = process.env.SPOTIFY_REFRESH_TOKEN;
   const client_id = process.env.SPOTIFY_CLIENT_ID;
   const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
-
   const auth = Buffer.from(`${client_id}:${client_secret}`).toString("base64");
 
+  // novo access_token
   const tokenResponse = await fetch("https://accounts.spotify.com/api/token", {
     method: "POST",
     headers: {
@@ -17,12 +23,12 @@ export default async function handler(req, res) {
   const tokenData = await tokenResponse.json();
   const access_token = tokenData.access_token;
 
+  // música atual
   const nowPlaying = await fetch("https://api.spotify.com/v1/me/player/currently-playing", {
     headers: { Authorization: `Bearer ${access_token}` },
   });
 
   const data = nowPlaying.status === 204 ? null : await nowPlaying.json();
-
   res.status(200).json(data);
 }
 
